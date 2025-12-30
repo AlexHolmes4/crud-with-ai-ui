fee91323fd4e382a803fe344df7790865f0790a3
Alex Holmes <00113969@uwa.edu.au>
2025-12-30 15:48:51 +0800

    service registration and pages

--- crud-with-ai-ui/crud-with-ai-ui/Program.cs
+++ crud-with-ai-ui/crud-with-ai-ui/Program.cs
@@ -1,11 +1,31 @@
 using crud_with_ai_ui;
+using crud_with_ai_ui.Services.Api;
+using crud_with_ai_ui.Services.PageServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOptions<ApiClientOptions>()
+    .Bind(builder.Configuration.GetSection(ApiClientOptions.SectionName));
+
+builder.Services.AddHttpClient<ConversationsApiService>((sp, client) =>
+{
+    var options = sp.GetRequiredService<IOptions<ApiClientOptions>>().Value;
+    client.BaseAddress = new Uri(options.BaseUrl);
+});
+
+builder.Services.AddHttpClient<ProductsApiService>((sp, client) =>
+{
+    var options = sp.GetRequiredService<IOptions<ApiClientOptions>>().Value;
+    client.BaseAddress = new Uri(options.BaseUrl);
+});
+
+builder.Services.AddScoped<ChatPageService>();
+builder.Services.AddScoped<ProductsPageService>();
+builder.Services.AddScoped<ProductDetailsPageService>();
 
 await builder.Build().RunAsync();
